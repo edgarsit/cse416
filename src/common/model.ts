79349a5c9aef40c9707b43af111aaaaca185c82c
Ayoub Benchaita <abenchaita@cs.stonybrook.edu>
2021-04-16 00:39:24 -0400

    Degree requirments class

--- src/common/model.ts
+++ src/common/model.ts
@@ -54,13 +54,79 @@ function rprop(options?: Omit<OptionsI, 'required'>, kind?: WhatIsIt): PropertyD
     short: '', long: '', ...options, required: true,
   }, kind);
 }
+export class XCreditsForCourseY{
+  @rprop()
+  public credits!: number
 
-export class xCreditsGradeY {
+  @rprop()
+  public course!: string
+
+}
+
+export class MaxCreditsForCourseY{
+  @rprop()
+  public credits!: number
+
+  @rprop({ type: () => [Course] })
+  public courses!: Course[]
+}
+
+
+export class XCreditsGradeY {
+  @rprop()
+  public credits!: number
+
+  @rprop()
+  public courses!: string
+}
+
+export class MaxCreditsForGradeY {
   @rprop()
   public credits!: number
 
   @rprop()
   public grade!: string
+}
+
+export class minCourcesSubArea {
+  @rprop()
+  public minCourses!: number
+
+  @rprop({ type: () => [SubArea] })
+  public subArea!: SubArea[]
+}
+
+export class RegularLectureBasedCourses{
+  @rprop()
+  public minCourses!: number
+
+  @rprop({ type: () => [Course] })
+  public notCountedCourses!: Course[]
+
+  @rprop({ type: () => [Course] })
+  public courawaOnlyOnce!: Course[]
+
+}
+
+
+export class RequiredCreditsOfCourseX{
+  @rprop()
+  public minCredits!: number
+
+  @rprop()
+  public maxCredits!: number
+
+  @rprop({ type: () => [Course] })
+  public substitutionsWithApproval!: Course[]
+
+}
+
+export class SubArea {
+  @rprop()
+  public name!: string
+
+  @rprop({ type: () => [Course] })
+  public courses!: Course[]
 }
 
 export class DegreeRequirements {
@@ -85,8 +151,8 @@ export class DegreeRequirements {
   @rprop()
   public creditsNeededToGraduate!: number
 
-  @rprop({ ref: () => xCreditsGradeY })
-  public prerequisites!: xCreditsGradeY
+  @rprop({ ref: () => XCreditsGradeY })
+  public prerequisites!: XCreditsGradeY
 
   @rprop({ type: () => [Course] })
   public coreCourses!: Course[]
@@ -103,6 +169,8 @@ export class CoursePlanComment {
   public text!: string
 }
 
+
+
 export class Elective {
   @rprop()
   public numCourses!: number
@@ -118,11 +186,39 @@ export class Elective {
 }
 
 export class Tracks {
+
+  @rprop()
+  public name!: String
+
   @rprop()
   public totalCredits!: number
 
+  @rprop()
+  public thesisRequired!: boolean
+
+  @rprop({ type: () => XCreditsForCourseY })
+  public xCreditsForCourseY!: XCreditsForCourseY[]
+
+  @rprop({ type: () => XCreditsForCourseY })
+  public maxCreditsInCombinationY!: XCreditsForCourseY[]
+
+  @rprop({ type: () => MaxCreditsForCourseY })
+  public maxCreditsForCourseY!: MaxCreditsForCourseY
+
   @rprop({ type: () => [Course] })
   public coreCourses!: Course[]
+
+  @rprop({ type: () => minCourcesSubArea })
+  public minOneCourseInEachSubArea!: minCourcesSubArea
+
+  @rprop({ type: () => minCourcesSubArea })
+  public minTwoCourseInEachSubArea!: minCourcesSubArea
+
+  @rprop({ type: () => RegularLectureBasedCourses })
+  public minRegularLectureCourses!: RegularLectureBasedCourses
+
+  @rprop({ type: () => RequiredCreditsOfCourseX })
+  public requiredCreditsOfCourseX!: RequiredCreditsOfCourseX
 
   @rprop({ type: () => Elective })
   public electives!: Elective
