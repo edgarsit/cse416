4ec453ca2c0a2734f351915f56b8ecbee8861e67
edgarsit <56287602+edgarsit@users.noreply.github.com>
2021-05-07 11:12:09 -0400

    remove integrity from link

--- src/server/index.ts
+++ src/server/index.ts
@@ -29,7 +29,6 @@ const html = (body: string, val?: any, url = 'client') => {
     <link
       rel="stylesheet"
       href="/public/bootstrap.min.css"
-      integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
       crossorigin="anonymous"
     />
     <link
